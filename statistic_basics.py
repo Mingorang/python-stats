375638f703afcfb7d595d2b17192b0b5acd786a0
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-06-26 12:51:06 +0100

    Add files via upload
    First project coded, uses the random module to  give n number of samples and calculates mean and standard deviation of results, when n is very large ( 100,000 +), and the range of values allowed is also large (1000+) ,the "random" distribution seems to follow a normal distribution centred halfway between the two limits. I also added a cross-hair, the colour can be changed.

---
+++ statistic_basics.py
@@ -0,0 +1,40 @@
+import random 
+import math
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import numpy as np
+import matplotlib.widgets
+
+
+
+#Used for running loops from sample size
+runs = 0
+nums = []
+nums2 = []
+index = 0
+
+n = int(input("How many samples should there be: "))
+a=0
+b=100
+while runs <= (n-1):
+    number = random.randrange(a,b,1)
+    nums.append(number)
+    nums2.append(number*number)
+    runs += 1
+
+
+mean = sum(nums)/n
+sum(nums2)
+std = math.sqrt((sum(nums2)/n)  - (mean*mean))
+print("mean is: ", mean.__round__(4))
+print("standard deviation is: ", std.__round__(4))
+
+x=np.linspace(a, b,n)
+y=norm(loc=mean, scale = std).pdf(x)
+fig,ax = plt.subplots(figsize=(20, 10))
+ax.set_snap(True)
+cursor = matplotlib.widgets.Cursor(ax, useblit=True, horizOn=True, vertOn=True, color="lightgreen")
+
+fig.canvas
+plt.plot(x,y)
+plt.show()
