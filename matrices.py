05d3c213a4ab49b82bf2ceb00358970119a05819
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-05 13:55:22 +0100

    Enhance matrices.py with visualization and corrections
    Added functionality to visualize matrices and updated comments., solving systems of linear equations with up to 4 variables. This result can be verified using https://www.desmos.com/3d =, might add an interactive 3d graph at some point

--- matrices.py
+++ matrices.py
@@ -1,23 +1,59 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+#Maybe use to solve large systems of simultaneous equations?
+
+
+import numpy as np
 from scipy.linalg import solve
 
 size = int(input("What is the size of the simultaneous equation? (2-4): "))
+
 if size == 2:
     print("Matrix A will be a 2 x 1 matrix, so matrix b will have 2 variables x and y.")
 elif size == 3:
     print("Matrix A will be a 3 x 1 matrix, so matrix b will have 3 variables x, y and z.")
 elif size == 3:
     print("Matrix A will be a 4 x 1 matrix, so matrix b will have 4 variables x, y, z and t.")
+
 #Eqauations are of form Ax=b
 A = []
 b = []
 
-#Finding matrix A (coefficient matrix)
+#Finding matrix A (transformation matrix)
 for i in range(size):
     row = []
+    print("")
     for j in range(size):
-        coefficient = float(input("Coefficient at specific point: "))
-        row.append(coefficient)
+        transform = float(input("Coefficient at specific point: "))
+        row.append(transform)
     A.append(row)
 A = np.array(A)
 print(A)
+
+#Finding matrix b (position vector), same method as matrix A
+for k in range(size):
+    row_1 = []
+    position = float(input("Value at the right of the = sign: "))
+    row_1.append(position)
+    b.append(row_1)
+b = np.array(b)
+print(b)
+#Solution using scipy, calculating inverse of A can introduce errors especially when using floats as coefficients
+solution = solve(A,b)
+
+if size ==2:
+    print(f"X = {solution[0].__round__(5)}")
+    print(f"Y = {solution[1].__round__(5)}")
+elif size == 3:
+    print(f"X = {solution[0].__round__(5)}")
+    print(f"Y = {solution[1].__round__(5)}")
+    print(f"Z = {solution[2].__round__(5)}")                           
+elif size == 4:
+    print(f"X = {solution[0].__round__(5)}")
+    print(f"Y = {solution[1].__round__(5)}")
+    print(f"Z = {solution[2].__round__(5)}")
+    print(f"T = {solution[3].__round__(5)}")
+
