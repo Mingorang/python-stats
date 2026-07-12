ff163f5903fc960c78cea31d64bb8d8ac323e623
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-12 13:32:01 +0100

    Implement ODE solver and visualization
    Added a function to compute the derivative and plot solutions using both odeint and solve_ivp. Coupled ODES will be added soon.

--- ode_solver.py
+++ ode_solver.py
@@ -3,3 +3,20 @@ import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
+
+# In form dy/dx = f(x,y) --> derivative is a function involving x and y
+
+def dydx(x,y):
+    return x
+v0=0
+x=np.linspace(-6,-1,100)
+sol_m1 = odeint(dydx, y0=v0, t=x, tfirst=False)
+sol_m2 = solve_ivp(dydx, t_span=(-10,max(x)), y0=[v0], t_eval=x)
+v_sol_m1 = sol_m1.T[0]
+v_sol_m2 = sol_m2.y[0]
+
+plt.plot(x,v_sol_m1)
+plt.plot(x,v_sol_m2, '--')
+plt.ylabel('$f(x)$')
+plt.xlabel('$x')
+plt.show()
