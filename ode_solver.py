7c75f7e883289446ceaefb018445679ddbb72d7c
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-14 17:56:36 +0100

    Modify derivative function and improve plot details
    Updated the derivative function and adjusted the x range for better visualization. Enhanced plotting with labels and legends.

--- ode_solver.py
+++ ode_solver.py
@@ -7,16 +7,18 @@ from scipy.integrate import solve_ivp
 # In form dy/dx = f(x,y) --> derivative is a function involving x and y
 
 def dydx(x,y):
-    return x
+    return 1
 v0=0
-x=np.linspace(-6,-1,100)
-sol_m1 = odeint(dydx, y0=v0, t=x, tfirst=False)
+x=np.linspace(-10, 10,1000)
+
+sol_m1 = odeint(dydx, y0=v0, t=x, tfirst=True)
 sol_m2 = solve_ivp(dydx, t_span=(-10,max(x)), y0=[v0], t_eval=x)
 v_sol_m1 = sol_m1.T[0]
 v_sol_m2 = sol_m2.y[0]
 
-plt.plot(x,v_sol_m1)
-plt.plot(x,v_sol_m2, '--')
+plt.plot(x,v_sol_m1, label="odeint solution")
+plt.plot(x,v_sol_m2, '--', label="solve_ivp solution")
+plt.legend()
 plt.ylabel('$f(x)$')
-plt.xlabel('$x')
+plt.xlabel('$x$')
 plt.show()
