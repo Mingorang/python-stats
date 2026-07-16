3563386ec6862bdff029d5abac50a5028b13439a
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-16 13:26:37 +0100

    added comments to understand code and made calculations more accurate

--- bino.py
+++ bino.py
@@ -1,22 +1,25 @@
+#imports
 import math
 import matplotlib.pyplot as plt
 from math import factorial
-import numpy as np
 import sys
-#binomial distribution an m sided dice with a (1/m) probability of landing on the mth side, all repeated n times
 
+#User inputs to find some binomial distribution
 n = int(input("Number of trials: "))
 p = float(input("Probability of success (0-1): "))
 x = int(input("How many successes happened: "))
-
-if x>n or (p>1 or p<0) :
-    sys.exit()
-
 bino_cdf=0
 k=0
+#PDF is the probability of success at one point, CDF is the total successes from 0 to some point x.
+
+#Logic part to check whether the distribution function is actually posiible with user input
+if x>n or (p>1 or p<0) :
+    sys.exit("Invalid: successes cannot be greater than the number of trials and probability of successes must be in the range 0 to 1.")
+#Looping to find the total and the initial PDF from user input, binomial is discrete so goes up by 1    
 while k <= x:
     bino_pdf = (factorial(n) / (factorial(k) * factorial(n - k))) * (p**k) * ((1 - p)**(n - k))
     bino_cdf += bino_pdf
-    print(f"Successes (k): {k} | PDF: {bino_pdf:.8f} | Running CDF: {bino_cdf:.8f}")
+    print(f"Successes (k): {k} | PDF: {bino_pdf:.20f} | Running CDF: {bino_cdf:.20f}")
     k += 1
-print(f"\nFinal Cumulative Distribution Function (CDF): {bino_cdf:.8f}")
+print(f"\nFinal Cumulative Distribution Function (CDF): {bino_cdf:.16f}")
+print(f"\nFinal Probability Density Function (PDF): {(factorial(n) / (factorial(x) * factorial(n - x))) * (p**x) * ((1 - p)**(n - x)):.16f}")
