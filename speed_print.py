eb28bb55d827ba5ae97ebb78a623c6fbe0a2dcc0
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-16 22:28:58 +0100

    Enhance speed_print.py with random timing and mean calculation
    Add functionality to measure and print mean numbers per second. Will add a better means of performance, like a standard index at some point

--- speed_print.py
+++ speed_print.py
@@ -1,8 +1,25 @@
 import time 
+import random
+import math
+alpha = []
+for a in range(0,100):
+    beta=[]
+    upper=int(random.randrange(0,10**3,1))/(10**6)
+    for i in range(0,10):
+        start = time.time()
 
-start = time.time()
-k = int(input("Choose a number: "))
-for i in range(0,10**k):
-    print(i)
-    i+=1
-end = time.time()
+
+        k=0
+        while (time.time()-start) < upper:
+            k+=1
+            pass
+        beta.append(k.__trunc__())
+        #print(f"Number reached in: {time.time()-start:.6} seconds is {k}")
+        i+=1
+
+    #print(sum(beta))
+    print(f"Mean numbers printed per second is {sum(beta)/upper}")
+    alpha.append(sum(beta)/upper)
+
+print(max(alpha))
+#Need to add matplot lib and difficult part will be introducing regressions, adding a list of reasonable methods similar to lines 9-25 of num_int.
