4dd472324f676298d3a745502db34923a9cbbbc8
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-07-16 23:01:00 +0100

    Refactor speed_print.py for larger range and structure
    added another loop to get the second maximum of looped values, using a smaller rand.randrange greatly increases, up to 70 million/s

--- speed_print.py
+++ speed_print.py
@@ -1,25 +1,30 @@
 import time 
 import random
 import math
-alpha = []
-for a in range(0,100):
-    beta=[]
-    upper=int(random.randrange(0,10**3,1))/(10**6)
-    for i in range(0,10):
-        start = time.time()
+biggest = []
+for a in range(0,1000):
+    alpha = []
+    for b in range(0,10):
+        beta=[]
+        upper=int(random.randrange(1,10**2,1))/(10**6)
+        for c in range(0,10):
+            start = time.time()
 
 
-        k=0
-        while (time.time()-start) < upper:
-            k+=1
-            pass
-        beta.append(k.__trunc__())
-        #print(f"Number reached in: {time.time()-start:.6} seconds is {k}")
-        i+=1
+            k=0
+            while (time.time()-start) < upper:
+                k+=1
+                pass
+            beta.append(k)
+            #print(f"Number reached in: {time.time()-start:.6} seconds is {k}")
+            c+=1
 
-    #print(sum(beta))
-    print(f"Mean numbers printed per second is {sum(beta)/upper}")
-    alpha.append(sum(beta)/upper)
+        #print(sum(beta))
+        #print(f"Mean numbers printed per second is {sum(beta)/upper}")
+        alpha.append(sum(beta)/upper)
 
-print(max(alpha))
+    print(max(alpha))
+    biggest.append(max(alpha))
+print("")
+print(max(biggest))
 #Need to add matplot lib and difficult part will be introducing regressions, adding a list of reasonable methods similar to lines 9-25 of num_int.
