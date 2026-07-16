311b316f91f6863023e8162b6dc4e3d4f9830696
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-16 11:32:37 +0100

    Refactor print statements and import for solving equations
    Updated print statements to show full precision for solution values. Added import statement for scipy.linalg.solve.

--- matrices.py
+++ matrices.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.linalg import solve
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
@@ -7,9 +6,11 @@ from mpl_toolkits import mplot3d
 #Maybe use to solve large systems of simultaneous equations?
 
 
+import numpy as np
+from scipy.linalg import solve
 
 size = int(input("What is the size of the simultaneous equation? (2-4): "))
-
+print("")
 if size == 2:
     print("Matrix A will be a 2 x 1 matrix, so matrix b will have 2 variables x and y.")
 elif size == 3:
@@ -39,20 +40,19 @@ for k in range(size):
     row_1.append(position)
     b.append(row_1)
 b = np.array(b)
-print(b)
 #Solution using scipy, calculating inverse of A can introduce errors especially when using floats as coefficients
 solution = solve(A,b)
 
 if size ==2:
-    print(f"X = {solution[0].__round__(5)}")
-    print(f"Y = {solution[1].__round__(5)}")
+    print(f"X = {solution[0]}")
+    print(f"Y = {solution[1]}")
 elif size == 3:
-    print(f"X = {solution[0].__round__(5)}")
-    print(f"Y = {solution[1].__round__(5)}")
-    print(f"Z = {solution[2].__round__(5)}")                           
+    print(f"X = {solution[0]}")
+    print(f"Y = {solution[1]}")
+    print(f"Z = {solution[2]}")                           
 elif size == 4:
-    print(f"X = {solution[0].__round__(5)}")
-    print(f"Y = {solution[1].__round__(5)}")
-    print(f"Z = {solution[2].__round__(5)}")
-    print(f"T = {solution[3].__round__(5)}")
+    print(f"X = {solution[0]}")
+    print(f"Y = {solution[1]}")
+    print(f"Z = {solution[2]}")
+    print(f"T = {solution[3]}")
 
