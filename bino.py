a922d24a053b11fe4b9c1317bf2df33b59fcc169
Mingorang <145214320+Mingorang@users.noreply.github.com>
2026-07-16 13:14:15 +0100

    dealing with binomial logic errors by ending program

--- bino.py
+++ bino.py
@@ -2,11 +2,16 @@ import math
 import matplotlib.pyplot as plt
 from math import factorial
 import numpy as np
+import sys
 #binomial distribution an m sided dice with a (1/m) probability of landing on the mth side, all repeated n times
 
 n = int(input("Number of trials: "))
 p = float(input("Probability of success (0-1): "))
 x = int(input("How many successes happened: "))
+
+if x>n or (p>1 or p<0) :
+    sys.exit()
+
 bino_cdf=0
 k=0
 while k <= x:
