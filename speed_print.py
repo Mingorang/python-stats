c89fba8425e7baea604f84a0480f5d7a98ce9eeb
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-07-20 17:09:55 +0100

    Good work, (0 ai!!!) added a plot that shows up, still need to fix logic errors like graph not being the same as code

--- speed_print.py
+++ speed_print.py
@@ -1,12 +1,13 @@
 import time 
 import random
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 biggest = []
 for a in range(0,1000):
     alpha = []
     for b in range(0,10):
         beta=[]
-        upper=int(random.randrange(1,10**2,1))/(10**6)
+        upper=int(random.randrange(1,2,1))/(10**6)
         for c in range(0,10):
             start = time.time()
 
@@ -30,4 +31,9 @@ print(max(biggest))
 #Need to add matplot lib and difficult part will be introducing regressions, adding a list of reasonable methods similar to lines 9-25 of num_int.
 
 #Just plotting atm
-plt.xsc
+x=np.logspace(start=-3, stop=3, num=int(str(len(biggest))))
+plt.scatter(x,biggest)
+plt.title("Big O notation")
+plt.xlabel("time taken between iterations")
+plt.ylabel("Number of iterations reached")
+plt.show()
