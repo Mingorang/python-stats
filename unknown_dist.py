a342b2ad508deda448ca2ae0925ba5aac9b217eb
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-07-24 09:14:38 +0100

    defined all the paramters of every distribution i know

--- unknown_dist.py
+++ unknown_dist.py
@@ -1,2 +1,7 @@
-#similar to the binomial distribution, will write code for poisson, geometric and negative geometric
+#similar to the binomial distribution, will write code for poisson, geometric and negative binomial
 #Should condense all probability distributions into this one
+#Normal: mean(μ) and standard deviation(σ)
+#Binomial: number of trials(n), probability of success(p)
+#Poisson: events happen once at a time, randomly and independent of each other and happen at a constant rate; only 1 parameter λ (lambda)
+#Geometric: number of trials(x)  until success with a probability(p) of success.
+#Negative binomial: NB(r,p) --> trials up until the rth success, with a set probability (p)
