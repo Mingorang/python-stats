ac35579c89e722ca1ac4fd2de6705bad3eba1487
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-07-24 09:31:39 +0000

    code still doesnt work, will finish normal dist and debug only dist for today

--- unknown_dist.py
+++ unknown_dist.py
@@ -17,8 +17,8 @@ print("Poisson ")
 print("Geometric ")
 print("Negative binomial")
 choice = int(input("Choose a dist: "))
-pdfcdf = bool(input("CDF or PDF (y/n)"))
- if choice == 1:
+pdfcdf = bool(input("CDF or PDF (y/n): "))
+if choice == 1:
     n = int(input("Number of trials: "))
     p = float(input("Probability of success (0-1): "))
     x = int(input("How many successes happened: "))
@@ -42,11 +42,11 @@ elif choice == 2:
     μ = float(input("Choose a value for mean: "))     
     x = float(input("no. of successes / occurences: "))
     cdf_norm = 0
-    if pdfcdf == 'y'
-    a = float(input("Lower limits"))
-    step = abs(x-a)/1000
-    while a <= x:
-    pdf_norm = ((1/(σ*math.sqrt(2*math.pi))))*((math.e)**(-0.5*((x-μ)/σ)**2))
-    cdf_norm += pdf_norm
-    print(f"Occurences (x): {x} | PDF: {pdf_norm:.20f} | Running CDF: {cdf_norm:.20f}")
-    a+= step
+    if pdfcdf == True:
+        a = float(input("Lower limits"))
+        step = abs(x-a)/1000
+        while a <= x:
+            pdf_norm = ((1/(σ*math.sqrt(2*math.pi))))*((math.e)**(-0.5*((x-μ)/σ)**2))
+            cdf_norm += pdf_norm
+            print(f"Occurences (a): {a:.6f} | PDF: {pdf_norm:.20f} | Running CDF: {cdf_norm:.20f}")
+            a+= step
