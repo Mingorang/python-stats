7ec3f6bb5baf0d6ed90011ddd2d5e93496d5e4f7
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-07-31 09:19:20 +0000

    data randomiser

--- usability2/confidence_interv.py
+++ usability2/confidence_interv.py
@@ -1,6 +1,20 @@
-import csv
+import random as rand
+import numpy as np
+import sys
+def data_randomiser(n):
+    #Generates 'n' random numbers between 0 and 10000
+    range = int(input("size of max value: "))
+    if range < 1:
+        sys.exit("Bad range")
 
-with open("all_seasons.csv", newline="") as csvfile:
-    height_taker = csv.reader(csvfile,delimiter='.', quotechar='e' )
-    for row in height_taker:
-        print(', '.join(row))
+    data = []
+    for i in range(n):
+        data.append(rand.randint(0, range))
+    return data
+print(data_randomiser(10))
+
+k=3000
+
+def bootsrap(data, n):
+    sample_range = round(range/k)
+    
