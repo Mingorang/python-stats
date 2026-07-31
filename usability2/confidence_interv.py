d0acf88f246bb0e6c97879167b6e4f853c67d0be
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-07-31 08:51:38 +0000

    works now, had to change the cwd

--- usability2/confidence_interv.py
+++ usability2/confidence_interv.py
@@ -1,6 +1,6 @@
 import csv
 
 with open("all_seasons.csv", newline="") as csvfile:
-    height_taker = csv.reader(csvfile,delimiter='', quotechar=' e' )
+    height_taker = csv.reader(csvfile,delimiter='.', quotechar='e' )
     for row in height_taker:
         print(', '.join(row))
