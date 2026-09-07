70d9a9d2b488f14caa272745d14491b266485f97
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-09-07 19:21:21 +0100

    Update comment to clarify stock price modeling
    update after a long time doing nothing useful

---
+++ usability2/price_modeling.py
@@ -0,0 +1,2 @@
+#From a youtube video, assume price of a stock is n, n ranges from 85 to 115, from the random generation we use a normal distribution about n and pick some distance away from n to be the price.
+#This updates 50 tines per second on a real time graph and shows the high,low,open and high as a 'candle'
