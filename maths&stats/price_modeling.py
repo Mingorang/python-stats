631c760929a19bd8f8830c1f8d1efdb5af7dbf3d
Ayomide Ehindero <145214320+Mingorang@users.noreply.github.com>
2026-09-10 13:07:09 +0000

    added csv file to read to

--- maths&stats/price_modeling.py
+++ maths&stats/price_modeling.py
@@ -3,11 +3,25 @@ import time
 import pandas as pd
 import mplfinance as mpf
 import matplotlib
+import csv
+import os
 
 #From a youtube video, assume price of a stock is n, n can range from 85 to 115, we then use another randomised factor to simulate price movement of a stock over time
 
-for i in range(50):
-    n = 100+(rand.randint(-15,15))
-    print(n)
-    n+=1
-    time.sleep(1/50)
+master_dir = os.path.dirname(os.path.abspath(__file__))
+folder_name = os.path.join(master_dir, "my_data_folder")
+os.makedirs(folder_name, exist_ok=True)
+filename = os.path.join(folder_name, "generated_data.csv")
+with open(filename, mode="a", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+
+    for i in range(50):
+        n = 100 + rand.randint(-15, 15)
+        print(n)
+        writer.writerow([n])  # Writes immediately to file
+        time.sleep(1 / 50)
+daily = pd.read_csv('maths&stats/my_data_folder/generated_data.csv',index_col=0,parse_dates=True)
+daily.index.name = "Date"
+daily.shape
+daily.head(3)
+daily.tail(3)
